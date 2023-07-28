c8e3f25256a4772731d8de73d4f79fe7738093da
Denis B <bulavatsky@list.ru>
2023-07-28 09:27:43 +0300

    add task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  const discriminant = Math.pow(b, 2)-4*a*c
+
+  if(discriminant < 0) {
+    arr = [];
+  } else if (discriminant = 0) {
+    const root = -b/(2*a);
+    arr = [root];
+  } else {
+    const rootOne = (-b + Math.sqrt(d) )/(2*a);
+    const rootTwo = (-b - Math.sqrt(d) )/(2*a);
+    arr = [rootOne, rootTwo];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
