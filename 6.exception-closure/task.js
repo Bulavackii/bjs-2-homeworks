cf066fdb8074d718e69278c0b6852dd3501c1ce3
Denis B <bulavatsky@list.ru>
2023-08-13 18:18:45 +0300

    ready task 2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -17,3 +17,44 @@ function validateCount(value) {
         return error; // Возвращаем ошибку из функции в случае перехвата исключения
     }
 }
+
+//=====================================================================================
+
+// Task 2.
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует"); // Генерируем ошибку
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c; // Периметр
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)); // формула Герона
+        return area.toFixed(3); // Округление до трёх знаков после запятой
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c); // Попытка создания треугольника
+    } catch (error) {
+        // В случае перехвата исключения, возвращаем объект с геттерами
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
