795c76150a1bf6ec61f32194d712d3dbbd1f5c91
Denis B <bulavatsky@list.ru>
2023-08-13 18:00:19 +0300

    ready task 1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,19 @@
-﻿//Task №1.
+﻿// Task 1.
+
+function parseCount(value) {
+    const parsedValue = Number.parseFloat(value); // Попытка парсинга строки в число
+
+    if (isNaN(parsedValue)) { // Если результат парсинга NaN
+        throw new Error("Невалидное значение"); // Генерируем ошибку
+    }
+
+    return parsedValue; // Возвращаем результат парсинга
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value); // Попытка парсинга значения
+    } catch (error) {
+        return error; // Возвращаем ошибку из функции в случае перехвата исключения
+    }
+}
