20a40a5351dbd0fe96f45ed94c15eba13f48b4b6
Denis B <bulavatsky@list.ru>
2023-08-13 12:06:17 +0300

    ready task 1

---
+++ 5.classes/task.js
@@ -0,0 +1,75 @@
+//Task №1.
+
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100; // по умолчанию состояние 100
+      this.type = null; // по умолчанию тип null
+    }
+  
+    // Метод для подклейки и улучшения состояния
+    fix() {
+      this.state *= 1.5;
+    }
+  
+    // Сеттер для свойства state с проверкой значений
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    }
+  
+    // Геттер для свойства state
+    get state() {
+      return this._state;
+    }
+  }
+  
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = "magazine"; // тип журнала
+    }
+  }
+  
+  class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.author = author; // имя автора книги
+      this.type = "book"; // тип книги
+    }
+  }
+  
+  class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "novel"; // тип романа
+    }
+  }
+  
+  class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "fantastic"; // тип фантастики
+    }
+  }
+  
+  class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "detective"; // тип детектива
+    }
+  }
+  
+
+//=============================================================
+
+  //Task №2.
+
+  
